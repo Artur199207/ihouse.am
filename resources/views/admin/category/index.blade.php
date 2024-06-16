2e4de644241708b3a817773e5bf99dfd0b9fc60b
Artur199207 <artcomputers92@gmail.com>
2024-06-17 03:50:11 +0400

    jjj

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -6,52 +6,45 @@
 <div class="container-fluid px-4">
     <div class="card mt-4">
         <div class="card-header">
-        <h4>View Category <a href="{{ url('admin/add-category') }}" class="btn btn-primary btn-sm float-end">Add category</a></h4>
+            <h4>View Category <a href="{{ url('admin/add-category') }}" class="btn btn-primary btn-sm float-end">Add
+                    category</a></h4>
 
         </div>
         <div class="card-body">
-        @if (session('message'))
-    <div class="alert alert-success">{{session('message')}}</div>
-    @endif
+            @if (session('message'))
+                <div class="alert alert-success">{{session('message')}}</div>
+            @endif
         </div>
     </div>
-    <table class="table table-bordered">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Category Name</th>
-            <th>Image</th>
-            <th>Status</th>
-            <th>Edit</th>
-            <th>Delete</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($category as $item)
-        <tr>
-            <td>{{ $item->id }}</td>
-            <td>{{ $item->name }}</td>
-            <td>
-        <img src="{{ asset('uploads/category/' . $item->image) }}" width="50px" height="50px" alt="ihouse">
-    
-</td>
-            <td>{{ $item->status == '1' ? 'Hidden' : 'Show' }}</td>
-            <td>
-                <a href="{{ url('admin/edit-category/'.$item->id) }}" class="btn btn-success">Edit</a>
-            </td>
-            <td>
-            <a href="{{ url('admin/delete-category/'.$item->id) }}" class="btn btn-danger">Delete</a>
-
-                
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-
-    </tbody>
-  </table>
-   
-    
+    <table id="myDataTable" class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Category Name</th>
+                <th>Image</th>
+                <th>Status</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($category as $item)
+                <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>
+                        <img src="{{ asset('uploads/category/' . $item->image) }}" width="50px" height="50px" alt="ihouse">
+                    </td>
+                    <td>{{ $item->status == '1' ? 'Hidden' : 'Show' }}</td>
+                    <td>
+                        <a href="{{ url('admin/edit-category/' . $item->id) }}" class="btn btn-success">Edit</a>
+                    </td>
+                    <td>
+                        <a href="{{ url('admin/delete-category/' . $item->id) }}" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,9 +15,15 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <link rel="stylesheet" href="//cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css">
+
     <!-- Styles -->
     <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap5.css">
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <style>
+      
+    </style>
 </head>
 
 <body>
@@ -33,9 +39,18 @@
             @include('layouts.inc.admin-footer')
             </div>
     </div>
+    <script src="{{ asset('assets/js/jquery.slim.min.js') }}"></script>
 
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/js/scripts.js') }}"></script>
+    <script src="//cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap5.js"></script>
+<script>
+    $(document).ready(function(){
+        $('#myDataTable').DataTable();
+    });
+</script>
+
 
 </body>
 
