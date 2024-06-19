dff48b945cc11841c8e54743001c78de9af5d071
Artur199207 <artcomputers92@gmail.com>
2024-06-19 17:28:03 +0400

    kkk

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
-
+use App\Http\Controllers\EmailController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
@@ -17,6 +17,7 @@ Route::get('/',[App\Http\Controllers\Frontend\FrontendController::class, 'index'
 
 
 Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
+
     Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
     Route::get('category', [App\Http\Controllers\Admin\CategoryController::class, 'index']);
     Route::get('add-category', [App\Http\Controllers\Admin\CategoryController::class, 'create']);
@@ -25,8 +26,11 @@ Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
     Route::put('update-category/{category_id}', [App\Http\Controllers\Admin\CategoryController::class, 'update']);
     Route::get('delete-category/{category_id}', [App\Http\Controllers\Admin\CategoryController::class, 'destroy']);
 
-
+    
 });
+
+Route::get('/email', [App\Http\Controllers\EmailController::class, 'create']);
+    Route::post('/email', [App\Http\Controllers\EmailController::class, 'sendEmail'])->name('send.email');
 // Route::prefix('admin')->middleware('auth')->group(function() {
 //     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
 // });
