886b7fa11873487788aa1415af942712fda17dec
Artur199207 <artcomputers92@gmail.com>
2024-06-25 15:22:03 +0400

    kkk

--- resources/views/frontend/post/index.blade.php
+++ resources/views/frontend/post/index.blade.php
@@ -1,201 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/css/lightslider.min.css">
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 <style>
-  .content_swiper {
-    position: relative;
-    height: 100%;
-  }
-
-  .swiper {
-    width: 100%;
-    height: 100%;
-  }
-
-  .swiper-container {
-    position: relative;
-    overflow: hidden;
-    width: 100%;
-    height: 100%;
-  }
-
-  .swiper-wrapper {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-    display: flex;
-    transition-property: transform;
-    box-sizing: content-box;
-  }
-
-  .swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .swiper-slide img {
-    width: 100%;
-    height: auto;
-    max-height: 400px;
-    object-fit: cover;/
-  }
-
-
-
-  .swiper {
-    width: 48%;
-    height: 300px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-
-  .swiper-slide {
-    background-size: cover;
-    background-position: center;
-  }
-
-  .mySwiper2 {
-    height: 80%;
-    width: 48%;
-  }
-
-  .mySwiper {
-    height: 20%;
-    box-sizing: border-box;
-    padding: 10px 0;
-  }
-
-  .mySwiper .swiper-slide {
-    width: 25%;
-    height: 100%;
-    opacity: 0.4;
-  }
-
-  .mySwiper .swiper-slide-thumb-active {
-    opacity: 1;
-  }
-
-  .swiper-slide img {
-    display: block;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  .modal {
-    display: none;
-    position: fixed;
-    z-index: 9999;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    overflow: auto;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .modal-content {
-    position: relative;
-    background-color: #fefefe;
-    padding: 20px;
-    border: 1px solid #888;
-    overflow: auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 55%;
-  }
-
-  .content__btn {
-    width: 100%;
-    position: absolute;
-    display: flex;
-    justify-content: space-between;
-    padding-inline: 10px;
-  }
-
-  .modal-content img {
-    max-width: 57%;
-    max-height: 93%;
-    object-fit: contain;
-  }
-
-  .close {
-    position: absolute;
-    top: 15px;
-    right: 25px;
-    font-size: 30px;
-    font-weight: bold;
-    color: #aaa;
-    cursor: pointer;
-    z-index: 1;
-  }
-
-  .close:hover,
-  .close:focus {
-    color: black;
-    text-decoration: none;
-    cursor: pointer;
-  }
-
-  @media screen and (max-width:1200px) {
-    .modal-content {
-      width: 80%;
+  .demo {
+      width: 850px; /* Adjusted width */
+      margin: 0 auto; /* Center align the container */
     }
-
-    .swiper {
-      width: 80%;
+    .header>a{
+      display:inline-block;
     }
-
-  }
-
-  @media screen and (max-width:768px) {
-    .mySwiper2 {
-      height: 80%;
-      width: 80%;
-    }
-  }
-
-
-  @media screen and (max-width:678px) {
-    .modal-content {
-      width: 80%;
-      height: 80%;
-    }
-
-    .modal-content img {
-      max-width: 100%;
-      max-height: 100%;
-      object-fit: contain;
-    }
-
-    .content__btn {
+    .contnet_image {
+      display: block;
+      height: auto;
       width: 100%;
-      position: absolute;
-      display: flex;
-      justify-content: space-between;
-      padding-inline: 10px;
-      height: 100%;
-      align-items: flex-end;
-      margin-bottom: 20px;
     }
-  }
+    @media screen and (max-width:1210px) {
+      .hidden-xs{
+        display:none !important;
+      }
+      .demo{
 
-  @media screen and (max-width:500px) {
-    .modal-content {
-      width: 80%;
-      height: 45%;
+      }
     }
-
-  }
 </style>
 <div class="inside-banner">
   <div class="container">
@@ -260,39 +89,31 @@
 
 
 
-
       @if (!empty($category->image1))
-        @php
+    @php
         $images = json_decode($category->image1, true); 
-      @endphp
+    @endphp
 
-        <div class="content_swiper col-lg-9 col-sm-8">
-        <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
-          <div class="swiper-wrapper">
-          @foreach ($images as $image)
-        <div class="swiper-slide">
-        <img src="{{ asset($image) }}" alt="Category Image">
+    <div class="col-lg-9 col-sm-4">
+        <div class="demo">
+            <ul id="lightSlider">
+                @foreach ($images as $image)
+                    <li data-thumb="{{ asset($image) }}">
+                        <img src="{{ asset($image) }}" alt="Category Image" class='contnet_image'>
+                    </li>
+                @endforeach
+            </ul>
         </div>
-      @endforeach
-          </div>
-          <div class="swiper-button-next"></div>
-          <div class="swiper-button-prev"></div>
-        </div>
-        <div thumbsSlider="" class="swiper mySwiper">
-          <div class="swiper-wrapper">
-          @foreach ($images as $image)
-        <div class="swiper-slide">
-        <img src="{{ asset($image) }}" alt="Category Image">
-        </div>
-      @endforeach
-          </div>
-        </div>
-        </div>
+    </div>
 
-    @else
-      <p>No images available.</p>
-    @endif
+      
+@else
+    <div class="col-lg-9">
+        <p>No images available.</p>
+    </div>
+@endif
 
+     
 
 
 
@@ -313,90 +134,17 @@
 
 
   </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/js/lightslider.min.js"></script>
 
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
   <script>
-    var swiper = new Swiper(".mySwiper", {
-      loop: true,
-      spaceBetween: 10,
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesProgress: true,
-    });
-
-    var swiper2 = new Swiper(".mySwiper2", {
-      loop: true,
-      spaceBetween: 10,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      thumbs: {
-        swiper: swiper,
-      },
-    });
-
-    // Modal functionality
-    var modal = document.getElementById("myModal");
-    var modalImg = document.getElementById("modal-img");
-    var slides = document.querySelectorAll(".mySwiper2 .swiper-slide img");
-    var currentSlideIndex = 0;
-
-    // Function to show modal with specific slide
-    function showModal(index) {
-      modal.style.display = "flex";
-      modalImg.src = slides[index].src;
-      currentSlideIndex = index;
-    }
-
-    // Click event listeners for each slide in main swiper
-    slides.forEach(function (slide, index) {
-      slide.addEventListener("click", function () {
-        showModal(index);
-      });
-    });
-
-    // Close modal functionality
-    var closeModal = document.getElementsByClassName("close")[0];
-    closeModal.onclick = function () {
-      modal.style.display = "none";
-    };
-
-    // Close modal when clicking outside of the modal content
-    window.onclick = function (event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    };
-
-    // Next and Previous buttons
-    var nextButton = document.getElementById("nextBtn");
-    var prevButton = document.getElementById("prevBtn");
-
-    nextButton.addEventListener("click", function () {
-      currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-      showModal(currentSlideIndex);
-    });
-
-    prevButton.addEventListener("click", function () {
-      currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
-      showModal(currentSlideIndex);
-    });
-
-    // Keyboard navigation
-    document.addEventListener("keydown", function (event) {
-      if (modal.style.display === "flex") {
-        if (event.key === "ArrowRight" || event.key === "ArrowDown") {
-          currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-          showModal(currentSlideIndex);
-        } else if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
-          currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
-          showModal(currentSlideIndex);
-        } else if (event.key === "Escape") {
-          modal.style.display = "none";
-        }
-      }
-    });
+    $('#lightSlider').lightSlider({
+    gallery: true,
+    item: 1,
+    loop:true,
+    slideMargin: 0,
+    thumbItem: 9
+});
   </script>
 
   @endsection
